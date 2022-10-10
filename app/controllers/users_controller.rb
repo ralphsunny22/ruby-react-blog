492ede28adaa3a750bb6d33e74c8bc0e945cbc21
ralphsunny22 <ralphsunny114@gmail.com>
2022-10-10 14:30:20 +0100

    login user, token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     end
 
     def login
-        @user = User.find_by(name: user_params[:name])
+        @user = User.find_by(email: user_params[:email])
         if @user && @user.authenticate(user_params[:password])
             token = encode_token({ user_id: @user.id })
             render json: { user: @user, token: token }, status: :ok
